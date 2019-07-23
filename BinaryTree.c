b7d080fdddd34deac61a1ba72b230253e583721c
ayyappan <39662168+ayyappan3m@users.noreply.github.com>
2019-07-23 22:23:15 +0530

    Create BinaryTree.c

---
+++ BinaryTree.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct node *right;
+};
+struct Node* newNode(int data)
+{
+    struct Node *node = (struct Node*)malloc(sizeof(struct Node)*1);
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+};
+void add(struct Node* node,int data)
+{
+    if(node == NULL)
+        node = newNode(data);
+    else if(node->data > data)
+        add(node->left,data);
+    else if(node->data < data)
+        add(node->right,data);
+}
+void printInorder(struct Node* node)
+{
+    if(node == NULL)
+        return;
+    printf("%d\t",node->data);
+
+    printInorder(node->left);
+    printInorder(node->right);
+}
+void printPreorder(struct Node* node)
+{
+    if(node == NULL)
+        return;
+
+    printPreorder(node->left);
+    printf("%d\t",node->data);
+    printPreorder(node->right);
+}
+void printPostorder(struct Node* node)
+{
+    if(node == NULL)
+        return;
+
+    printPostorder(node->left);
+    printPostorder(node->right);
+    printf("%d\t",node->data);
+}
+
+int main()
+{
+    struct Node *root;
+    int choice;
+    int data;
+    add(root,20);
+    add(root,17);
+    add(root,13);
+    add(root,19);
+    add(root,29);
+    add(root,25);
+    add(root,30);
+    printf("%d->",root->data);
+    printf("%d->",root->left->data);
+    printf("%d->",root->right->data);
+    printf("%d->",root->left->left->data);
+    printf("%d->",root->left->right->data);
+    printf("%d->",root->right->left->data);
+    printf("%d->",root->right->right->data);
+
+    //printf("Hello world!\n %d",root->data);
+    do
+    {
+      printf("\t\tBinary Tree\n\n");
+      printf("1.Insert\n");
+      printf("2.Delete\n");
+      printf("3.Inorder\n");
+      printf("4.Preorder\n");
+      printf("5.postorder\n");
+      printf("Choice:");
+      scanf("%d",&choice);
+      if(choice<6)
+      {
+          switch(choice)
+          {
+            case 1: printf("Enter value to insert:");
+                    scanf("%d",&data);
+                    add(root,data);
+                    break;
+            case 2:
+                    break;
+            case 3: printf("Inorder:");
+                    printInorder(&root);
+                    break;
+            case 4: printf("Preorder:");
+                    printPreorder(&root);
+                    break;
+            case 5: printf("Postorder:");
+                    printPostorder(&root);
+                    break;
+
+          }
+      }
+      else
+      {
+          printf("Enter correct choice:");
+          getchar();
+      }
+    }while(choice!=6);
+    return 0;
+}
